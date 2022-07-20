fc6c7fa1002fbfe083b2f47ef46999593deb5005
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2022-07-20 23:29:42 +0200

    Logging: Remove Papertrail
    We do not use this functionality and cannot test it.
    It seems to be some cloud logging service - why would anyone want that?
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const config = require('config');
 const log = require('./src/app/log');
-const papertrail = require('./src/dao/papertrail');
-
-papertrail.init(config.papertrail);
 
 //
 // Start worker cluster depending on number of CPUs
